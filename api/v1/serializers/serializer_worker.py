3c04cb7c6c96d0c304e1722dc3ccad11dbc38c4f
Renntor <denis30510@gmail.com>
2024-12-31 13:22:02 +0300

    added migrations and changed serializer

--- api/v1/serializers/serializer_worker.py
+++ api/v1/serializers/serializer_worker.py
@@ -19,7 +19,7 @@ class TeamSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Worker
-        fields = ('id', 'full_name',)
+        fields = ('id', 'full_name', 'specialization')
 
 
 class WorkerSerializer(serializers.ModelSerializer):
